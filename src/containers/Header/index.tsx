56965ca09133df5d8ad7e620597fdbda412ed8e0
Narbek <coffee.dev.na@gmail.com>
2022-09-27 10:57:01 +0300

    register modal

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,7 +1,20 @@
 import s from "./style.module.scss";
 import g from "Src/App.module.scss";
+import RegisterModalForm from "Src/components/RegisterModalForm";
+import { useState } from "react";
+import Button from "Src/components/Button";
 
 const Header = () => {
+  const [isRegisterModalOpened, setIsRegisterModalOpened] = useState(false);
+
+  function openModal() {
+    setIsRegisterModalOpened(true);
+  }
+
+  function closeModal() {
+    setIsRegisterModalOpened(false);
+  }
+
   return (
     <header className={s.header}>
       <div className={g.container}>
@@ -11,12 +24,21 @@ const Header = () => {
           <nav className={s.navbar}>
             <ul className={s.navbarList}>
               <li className={s.navbarItem}>
-                <button className={g.btn}>Регистрация</button>
+                <Button className={g.btn}>Регистрация</Button>
               </li>
               <li className={s.navbarItem}>
-                <button className={`${g.btn} ${g.btnBlue} ${g.btnBordered}`}>
+                <Button
+                  className={`${g.btn} ${g.btnBlue} ${g.btnBordered}`}
+                  onClick={openModal}
+                  color="blue"
+                >
                   Войти
-                </button>
+                </Button>
+
+                <RegisterModalForm
+                  isOpen={isRegisterModalOpened}
+                  onClose={closeModal}
+                />
               </li>
             </ul>
           </nav>
